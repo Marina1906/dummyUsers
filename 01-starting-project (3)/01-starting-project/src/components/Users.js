e39e000a7e6a1a5b2710005b792d6370b5dd1826
Marina Makević <128428504+Marina1906@users.noreply.github.com>
2023-12-30 13:45:59 +0100

    Update Users.js

--- 01-starting-project (3)/01-starting-project/src/components/Users.js
+++ 01-starting-project (3)/01-starting-project/src/components/Users.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
+
+
 import User from './User';
 
 import classes from './Users.module.css';
@@ -8,6 +10,28 @@ const DUMMY_USERS = [
   { id: 'u2', name: 'Manuel' },
   { id: 'u3', name: 'Julie' },
 ];
+
+class Users extends Component {
+  constructor() {
+    this.state = {};
+  }
+  
+toggleUsersHandler () {
+
+}
+
+  render () {
+    return (
+      <div className={classes.users}>
+      <button onClick={toggleUsersHandler}>
+        {showUsers ? 'Hide' : 'Show'} Users
+      </button>
+      {showUsers && usersList}
+    </div>
+
+    );
+  }
+}
 
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
